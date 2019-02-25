44ae1800446d08380eab67605e2f96b84ebda205
Andreas Rudolph <andy@openindex.de>
2019-02-25 14:12:29 +0100

    improved validation script

--- apps/check.py
+++ apps/check.py
@@ -107,6 +107,72 @@ def check_headlines_file(markdown_file):
     return counter
 
 
+# Check images in markdown files.
+def check_images(markdown_files):
+    counter = 0
+    for markdown_file in markdown_files:
+        counter = counter + check_images_file(markdown_file)
+
+    return counter
+
+
+# Check images in a markdown file.
+def check_images_file(markdown_file):
+    with open(markdown_file, 'r') as md:
+        data = md.read()
+
+    counter = 0
+    line_nr = 0
+    error_found = False
+    in_code_block = False
+    figure_pattern = re.compile(r'{{<\s+figure\s+([^>]*)\s+>}}')
+    figure_pattern_src = re.compile(r'src\s*=\s*\"([^\"]*)\"')
+    for line in data.split('\n'):
+        line_nr = line_nr + 1
+        if line.startswith('```'):
+            if in_code_block:
+                in_code_block = False
+            else:
+                in_code_block = True
+
+        if in_code_block:
+            continue
+
+        errors = []
+        for figure in figure_pattern.finditer(line):
+            group = figure.group(0)
+            try:
+                image = next(figure_pattern_src.finditer(group)).group(1)
+            except StopIteration:
+                errors.append([group, 'missing src attribute'])
+                continue
+
+            if image is None or image.strip() == '':
+                errors.append([group, 'empty src attribute'])
+                continue
+
+            image_path = os.path.normpath(os.path.join(os.path.dirname(markdown_file), image))
+            if not os.path.isfile(image_path):
+                errors.append([group, 'image "%s" not found' % os.path.basename(image_path)])
+
+        # show errors for the current line
+        if len(errors) > 0:
+            if not error_found:
+                error_found = True
+                print()
+                cprint_green('-' * 70, True)
+                cprint_green(' Invalid images in %s' % markdown_file, True)
+                cprint_green('-' * 70, True)
+
+            for error in errors:
+                print()
+                cprint_green('at line %s (%s):' % (line_nr, error[1]))
+                print(error[0])
+                counter = counter + 1
+
+    return counter
+
+
 # Check links in markdown files.
 def check_links(markdown_files):
     counter = 0
@@ -137,12 +203,12 @@ def check_links_file(markdown_file):
         if in_code_block:
             continue
 
+        errors = []
         for link in link_pattern.finditer(line):
             # print(link.group(0))
             # print(link.group(1))
             group = link.group(0)
             target = link.group(1)
-            errors = []
 
             # don't validate external links
             if target.startswith('http://') or target.startswith('https://'):
@@ -168,20 +234,20 @@ def check_links_file(markdown_file):
             else:
                 errors.append([group, 'unsupported link'])
 
-            # show errors for the current line
-            if len(errors) > 0:
-                if not error_found:
-                    error_found = True
-                    print()
-                    cprint_green('-' * 70, True)
-                    cprint_green(' Invalid links in %s' % markdown_file, True)
-                    cprint_green('-' * 70, True)
+        # show errors for the current line
+        if len(errors) > 0:
+            if not error_found:
+                error_found = True
+                print()
+                cprint_green('-' * 70, True)
+                cprint_green(' Invalid links in %s' % markdown_file, True)
+                cprint_green('-' * 70, True)
 
-                for error in errors:
-                    print()
-                    cprint_green('at line %s (%s):' % (line_nr, error[1]))
-                    print(error[0])
-                    counter = counter + 1
+            for error in errors:
+                print()
+                cprint_green('at line %s (%s):' % (line_nr, error[1]))
+                print(error[0])
+                counter = counter + 1
 
     return counter
 
@@ -224,14 +290,18 @@ def main(path):
     # Check headlines in markdown files.
     count_headline_errors = check_headlines(markdown_files)
 
+    # Check images in markdown files.
+    count_image_errors = check_images(markdown_files)
+
     # Check links in markdown files.
     count_link_errors = check_links(markdown_files)
 
     # Print summary.
-    count_total_errors = count_headline_errors + count_link_errors
+    count_total_errors = count_headline_errors + count_image_errors + count_link_errors
     print('')
     cprint_white('=' * 70, True)
     cprint_white(' Found %s errors in headlines.' % count_headline_errors, True)
+    cprint_white(' Found %s errors in images.' % count_image_errors, True)
     cprint_white(' Found %s errors in links.' % count_link_errors, True)
     cprint_white(' Found %s errors in total.' % count_total_errors, True)
     cprint_white('=' * 70, True)
